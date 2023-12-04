aa7c6f9ff151efe5c60cad493b20eee5f8a8d127
enelson1001 <ednelson5080@gmail>
2023-12-04 12:20:33 -0700

    Intial release

---
+++ README.md
@@ -0,0 +1,128 @@
+# Rust ESP32S3 Lvgl Demo
+
+The purpose of this demo is to get lv-binding-rust (Lvgl) running on the ESP32S3 development board
+
+## Development Board
+Aliexpress ESP32-8048S070 - 7 inch 800x400 TN RGB with ESP32S3, 8M PSRAM, 16M Flash, 512KB SRAM
+
+## Overview
+This application shows how to use lv-binding-rust crate on a ESP32S3 device.  The program will display a clock time on a blue backgound screen.
+The clock time is a simulated time of 21:00 to 21::59 where the seconds are incremented each second and repeats the 00-59 seconds forever.
+
+
+## partition-table folder
+The partition-table folder contains a file called partitons.csv.  This file increases the default factory/app partiton from the default of 1M to 3M.  
+This allows us more space for our program and since the flash size is 16M this should not be a problem.  This file will be called when we flash the device.
+
+## custom-fonts folder
+The custom-fonts folder contains our custom fonts.  The customs fonts are converted from TTF fonts using lvgl online font converter at https://lvgl.io/tools/fontconverter.  I used https://ttfonts.net to find a font I liked and then downloaded the font.  In the lvgl-online-font-converter I used the font name plus the font size for the name of the font.  I chose Bpp of 2 bit-per-pixel and set the range of 0x30-0x3A since I only need numbers and the ":" character.  After clicking on "Convert" the file will be downloaded. I placed this downloaded file (*.c) into the custom-fonts folder.  Then I created a header file which has an extern to my *.c file, along with changing the ifndef and define names.  
+To use this custom font, I added ```LVGL_FONTS_DIR = {relative = true, value = "custom-fonts"}``` to my config.toml under [env].  This allows our font to be compiled when lvgl is compiled.
+
+## lvgl-configs folder
+The lvgl-configs folder holds the lv_config.h and lv_drv_conf.h files which are required by lvgl to compile.  Everything in lv_drv_conf.h file is set to 0 as I am not using the lvgl drivers.  I don't think I changed anything in the lv_conf.h file.
+
+## lcd_panel.rs file
+The LCD RGB display driver.
+
+## sdkconfig.defaults file
+Increased the main task stack size to 60000 to allow app to run without crashing.
+
+The following needs to be added for using PSRAM.
+```
+CONFIG_SPIRAM=y
+CONFIG_SPIRAM_MODE_OCT=y
+CONFIG_SPIRAM_SPEED_80M=y
+
+# Enabling the following configurations can help increase the PCLK frequency in the case when
+# the Frame Buffer is allocated from the PSRAM and fetched by EDMA
+CONFIG_SPIRAM_FETCH_INSTRUCTIONS=y
+CONFIG_SPIRAM_RODATA=y
+```
+
+## bindings.h file in project root directory
+This file contains additional bindings required for using esp_lcd_panel esp-idf component that is available on the ESP32S3 device.
+
+## Cargo.toml project file
+I added the following to the "dependencies" section.
+```
+esp-idf-hal = { version = "0.42.1" }
+esp-idf-sys = { version = "0.33.3" }
+
+cstr_core = "0.2.1"
+embedded-graphics-core = "0.4.0"
+
+lvgl = { git = "https://github.com/enelson1001/lv_binding_rust", version = "0.6.2", default-features = false, features = [
+    "embedded_graphics",
+    "unsafe_no_autoinit",
+] }
+
+lvgl-sys = { git = "https://github.com/enelson1001/lv_binding_rust", version = "0.6.2" }
+
+```
+
+You need to a 'package.metadata.esp-idf-sys' section so the bindings.h file will be include.
+```
+[package.metadata.esp-idf-sys]
+extra_components = [{ bindings_header = "bindings.h" }]
+
+```
+
+## config.toml
+To get lv-bindings-rust to comple and build I made the following changes to the config.toml file. 
+```
+[build]
+target = "xtensa-esp32-espidf"
+
+[target.xtensa-esp32-espidf]
+linker = "ldproxy"
+# runner = "espflash --monitor" # Select this runner for espflash v1.x.x
+runner = "espflash flash --monitor" # Select this runner for espflash v2.x.x
+rustflags = [
+    # Extending time_t for ESP IDF 5: https://github.com/esp-rs/rust/issues/110
+    "--cfg",
+    "espidf_time64",
+
+    # Added the following 2 entries so lvgl will build without getting string.h file not found
+    "--sysroot",
+    "/home/ed/.rustup/toolchains/esp/xtensa-esp32s3-elf/esp-13.2.0_20230928/xtensa-esp-elf/xtensa-esp-elf/include",
+]
+
+[unstable]
+build-std = ["std", "panic_abort"]
+
+[env]
+MCU = "esp32"
+# Note: this variable is not used by the pio builder (`cargo build --features pio`)
+ESP_IDF_VERSION = "v5.1.1"
+
+# The directory that has the lvgl config files - lv_conf.h, lv_drv_conf.h
+DEP_LV_CONFIG_PATH = { relative = true, value = "lvgl-configs" }
+
+# Required to make lvgl build correctly otherwise get wrong file type
+CROSS_COMPILE = "xtensa-esp32-elf"
+
+# Directory for custom fonts (written in C) that Lvgl can use
+LVGL_FONTS_DIR = {relative = true, value = "custom-fonts"}
+```
+
+## lv-binding-rust fork
+I updated my fork of lv-binding-rust to include PR153 ie the changes recommended by madwizard-thomas.
+
+
+## Building the project
+The changes to config.toml, Cargo.toml and to build.rs allowed me to build lv-binding-rust without any errors. I do get 2 warnings about va_list not being FFI-safe.
+
+
+## Flashing the ESP32S3 device
+I used the following command to flash the ESP32S3 device.
+```
+$ cargo espflash flash --partition-table=partition-table/partitions.csv --monitor
+```
+
+## Picture of Aliexpress ESP32S3 running the demo
+![esp32s3-demo](photos/esp32s3-demo.jpg)
+
+
+# Versions
+### v1.0 : 
+- initial release
---
+++ src/main.rs
@@ -0,0 +1,218 @@
+// Place pub before mod otherwise youu will get warnings about multiple methods not used in lcd_panel
+pub mod lcd_panel;
+
+use log::*;
+
+use cstr_core::CString;
+
+use std::time::Instant;
+
+use esp_idf_hal::{delay, peripherals::Peripherals, units::FromValueType};
+
+use esp_idf_hal::ledc::{
+    config::TimerConfig,
+    {LedcDriver, LedcTimerDriver},
+};
+
+use lvgl;
+use lvgl::font::Font;
+use lvgl::style::Style;
+use lvgl::widgets::Label;
+use lvgl::{Align, Color, Display, DrawBuffer, Part, TextAlign, Widget};
+
+use crate::lcd_panel::{LcdPanel, PanelConfig, PanelFlagsConfig, TimingFlagsConfig, TimingsConfig};
+
+fn main() {
+    // It is necessary to call this function once. Otherwise some patches to the runtime
+    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
+    esp_idf_svc::sys::link_patches();
+
+    // Bind the log crate to the ESP Logging facilities
+    esp_idf_svc::log::EspLogger::initialize_default();
+
+    info!("=================== Starting APP! =========================");
+
+    const HOR_RES: u32 = 800;
+    const VER_RES: u32 = 480;
+    const LINES: u32 = 12; // The number of lines (rows) that will be refreshed
+
+    let peripherals = Peripherals::take().unwrap();
+
+    #[allow(unused)]
+    let pins = peripherals.pins;
+
+    //============================================================================================================
+    //               Create the LedcDriver to drive the backlight on the Lcd Panel
+    //============================================================================================================
+    let mut channel = LedcDriver::new(
+        peripherals.ledc.channel0,
+        LedcTimerDriver::new(
+            peripherals.ledc.timer0,
+            &TimerConfig::new().frequency(25.kHz().into()),
+        )
+        .unwrap(),
+        pins.gpio2,
+    )
+    .unwrap();
+    channel.set_duty(channel.get_max_duty() / 2).unwrap();
+    info!("Backlight turned on");
+
+    lvgl::init();
+
+    //============================================================================================================
+    //                         Create driver for the LCD Panel
+    //============================================================================================================
+    let mut lcd_panel = LcdPanel::new(
+        &PanelConfig::new(),
+        &PanelFlagsConfig::new(),
+        &TimingsConfig::new(),
+        &TimingFlagsConfig::new(),
+    )
+    .unwrap();
+
+    info!("=============  Registering Display ====================");
+    let buffer = DrawBuffer::<{ (HOR_RES * LINES) as usize }>::default();
+    let display = Display::register(buffer, HOR_RES, VER_RES, |refresh| {
+        lcd_panel
+            .set_pixels_lvgl_color(
+                refresh.area.x1.into(),
+                refresh.area.y1.into(),
+                (refresh.area.x2 + 1i16).into(),
+                (refresh.area.y2 + 1i16).into(),
+                refresh.colors.into_iter(),
+            )
+            .unwrap();
+    })
+    .unwrap();
+
+    //===========================================================================================================
+    //                               Create the User Interface
+    //===========================================================================================================
+    // Create screen and widgets
+    let mut screen = display.get_scr_act().unwrap();
+    let mut screen_style = Style::default();
+    screen_style.set_bg_color(Color::from_rgb((0, 0, 255)));
+    screen_style.set_radius(0);
+    screen.add_style(Part::Main, &mut screen_style);
+
+    let mut time = Label::new().unwrap();
+    let mut style_time = Style::default();
+    style_time.set_text_color(Color::from_rgb((255, 255, 255))); // white
+    style_time.set_text_align(TextAlign::Center);
+
+    // Custom font requires lvgl-sys in Cargo.toml and 'use lvgl_sys' in this file
+    style_time.set_text_font(unsafe { Font::new_raw(lvgl_sys::gotham_bold_80) });
+
+    time.add_style(Part::Main, &mut style_time);
+
+    // Time text will be centered in screen
+    time.set_align(Align::Center, 0, 0);
+
+    let mut i = 0;
+
+    loop {
+        let start = Instant::now();
+        if i > 59 {
+            i = 0;
+        }
+
+        let val = CString::new(format!("21:{:02}", i)).unwrap();
+        time.set_text(&val).unwrap();
+        i = 1 + i;
+
+        lvgl::task_handler();
+
+        // Simulate clock - so sleep for one second so time text is incremented in seconds
+        delay::FreeRtos::delay_ms(1000);
+
+        lvgl::tick_inc(Instant::now().duration_since(start));
+    }
+
+    /*
+        // This is an example of running Lvgl in its own thread.  You can reduce the mian task stack size in
+        // sdconfig.defaults back to 8000.
+
+        // Stack size value - 50,000 for 10 lines
+        let _lvgl_thread = thread::Builder::new()
+            .stack_size(50000)
+            .spawn(|| {
+                lvgl::init();
+
+                //============================================================================================================
+                //                         Create driver for the LCD Panel
+                //============================================================================================================
+                let mut lcd_panel = LcdPanel::new(
+                    &PanelConfig::new(),
+                    &PanelFlagsConfig::new(),
+                    &TimingsConfig::new(),
+                    &TimingFlagsConfig::new(),
+                )
+                .unwrap();
+                
+                info!("=============  Registering Display ====================");
+                let buffer = DrawBuffer::<{ (HOR_RES * LINES) as usize }>::default();
+                let display = Display::register(buffer, HOR_RES, VER_RES, |refresh| {
+                    lcd_panel
+                        .set_pixels_lvgl_color(
+                            refresh.area.x1.into(),
+                            refresh.area.y1.into(),
+                            (refresh.area.x2 + 1i16).into(),
+                            (refresh.area.y2 + 1i16).into(),
+                            refresh.colors.into_iter(),
+                        )
+                        .unwrap();
+                })
+                .unwrap();
+
+                //===========================================================================================================
+                //                               Create the User Interface
+                //===========================================================================================================
+                // Create screen and widgets
+                let mut screen = display.get_scr_act().unwrap();
+                let mut screen_style = Style::default();
+                screen_style.set_bg_color(Color::from_rgb((0, 0, 255)));
+                screen_style.set_radius(0);
+                screen.add_style(Part::Main, &mut screen_style);
+
+                let mut time = Label::new().unwrap();
+                let mut style_time = Style::default();
+                style_time.set_text_color(Color::from_rgb((255, 255, 255))); // white
+                style_time.set_text_align(TextAlign::Center);
+
+                // Custom font requires lvgl-sys in Cargo.toml and 'use lvgl_sys' in this file
+                style_time.set_text_font(unsafe { Font::new_raw(lvgl_sys::gotham_bold_80) });
+
+                time.add_style(Part::Main, &mut style_time);
+
+                // Time text will be centered in screen
+                time.set_align(Align::Center, 0, 0);
+
+                let mut i = 0;
+
+                loop {
+                    let start = Instant::now();
+                    if i > 59 {
+                        i = 0;
+                    }
+
+                    let val = CString::new(format!("21:{:02}", i)).unwrap();
+                    time.set_text(&val).unwrap();
+                    i = 1 + i;
+
+                    lvgl::task_handler();
+
+                    // Simulate clock - so sleep for one second so time text is incremented in seconds
+                    delay::FreeRtos::delay_ms(1000);
+
+                    lvgl::tick_inc(Instant::now().duration_since(start));
+                }
+            })
+            .unwrap();
+
+
+        loop {
+            // Don't exit application
+            delay::FreeRtos::delay_ms(1000);
+        }
+    */
+}
